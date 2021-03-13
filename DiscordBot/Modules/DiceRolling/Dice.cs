6b4503304cf797ea275d8de39e7a2bd91e702336
trameathia <trameathia@gmail.com>
2021-03-12 23:11:09 -0500

    can now add and subtract from dice rolls

--- DiscordBot/Modules/DiceRolling/Dice.cs
+++ DiscordBot/Modules/DiceRolling/Dice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text.RegularExpressions;
 
 namespace DiscordBot.Modules.DiceRolling
@@ -8,18 +9,25 @@ namespace DiscordBot.Modules.DiceRolling
     {
         private readonly int _rolls;
         private readonly int _sides;
-        public Dice(int rolls, int sides) => (_rolls, _sides) = (rolls, sides);
+        private readonly int _add;
+        public Dice(int rolls, int sides, int add) => (_rolls, _sides, _add) = (rolls, sides, add);
         public static bool TryParse(string input, out Dice dice)
         {
             dice = null;
+            int add = 0;
             if (!IsValidRoll(input))
                 return false;
             var temp = input.Split('d');
-            dice = new Dice(Convert.ToInt32(temp[0]), Convert.ToInt32(temp[1]));
+            var parts = Regex.Split(temp[1], @"[()+-]");
+            if (parts.Length > 1)
+                add = Convert.ToInt32(parts[1]);
+            if (temp[1].Contains('-'))
+                add = add * -1;
+            dice = new Dice(Convert.ToInt32(temp[0]), Convert.ToInt32(parts[0]), add);
             return true;
         }
-        private static bool IsValidRoll(string roll) => Regex.IsMatch(roll, @"^\d*d\d*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
+        private static bool IsValidRoll(string roll) => Regex.IsMatch(roll, @"^\d*d\d*[()+-]*\d*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        public int Add() => _add;
         public List<int> Roll()
         {
             List<int> rolls = new List<int>();
@@ -31,7 +39,7 @@ namespace DiscordBot.Modules.DiceRolling
         }
         public override string ToString()
         {
-            return $"{_rolls}d{_sides}";
+            return $"{_rolls}d{_sides}{(_add != 0 ? (_add > 0 ? "+"+_add : _add) : string.Empty)}";
         }
     }
 }
