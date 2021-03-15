1d5d996c4b2727494612c158b4ba3fde6112dd79
TJ <tjoshua07@outlook.com>
2021-03-15 17:41:39 -0400

    Implemented new utility class for misc helper methods.

---
+++ DiscordBot/Utility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace DiscordBot
+{
+	public static class Utility
+	{
+		public static string FormatTimeSpan(TimeSpan timespan) =>
+			string.Join(
+				" and ",
+				new (int Amount, string Verbiage)[]
+				{
+					(timespan.Days, "day"),
+					(timespan.Hours, "hour"),
+					(timespan.Minutes, "minute"),
+					(timespan.Seconds, "second")
+				}
+					.Where(timespanComponent => timespanComponent.Amount > 0)
+					.Take(2)
+					.Select(timespanComponent => $"{timespanComponent.Amount} {timespanComponent.Verbiage}{(timespanComponent.Amount > 1 ? "s" : string.Empty)}"));
+	}
+}
